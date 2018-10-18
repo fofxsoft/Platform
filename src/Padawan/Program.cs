882004feee275dc5fe94b1d1064075daa5e1a4f7
mkellsy <mkellsy@gmail.com>
2018-10-18 14:08:35 -0600

    initial commit

---
+++ src/Padawan/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using IdentityModel.Client;
+
+namespace Padawan
+{
+    class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            var disco = await DiscoveryClient.GetAsync("https://localhost:5000");
+
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+
+                return;
+            }
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("yoda");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            var client = new HttpClient();
+
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("https://localhost:5001/api/values");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                Console.WriteLine(JArray.Parse(await response.Content.ReadAsStringAsync()));
+            }
+        }
+    }
+}
---
+++ src/Singularity/Config.cs
@@ -0,0 +1,32 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Singularity
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("yoda", "Yoda")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "yoda" }
+                }
+            };
+        }
+    }
+}
