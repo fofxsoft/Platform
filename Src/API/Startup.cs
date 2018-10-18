253141bd82385b2fcb330beded8ff2cb315be4ac
mkellsy@gmail.com <8nfeNoUlS%sWXYz1Hcq1TSBH>
2018-10-18 15:01:59 -0600

    restructure

---
+++ Src/API/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PlatformOne.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration {
+            get;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore()
+                    .AddAuthorization()
+                    .AddJsonFormatters();
+
+            services.AddAuthentication("Bearer")
+                    .AddIdentityServerAuthentication(options =>
+                    {
+                        options.Authority = "https://localhost:5000";
+                        options.RequireHttpsMetadata = false;
+                        options.ApiName = "yoda";
+                    });
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
---
+++ Src/Console/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using IdentityModel.Client;
+
+namespace PlatformOne.Console
+{
+    class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            var disco = await DiscoveryClient.GetAsync("https://localhost:5000");
+
+            if (disco.IsError)
+            {
+                System.Console.WriteLine(disco.Error);
+
+                return;
+            }
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("yoda");
+
+            if (tokenResponse.IsError)
+            {
+                System.Console.WriteLine(tokenResponse.Error);
+
+                return;
+            }
+
+            System.Console.WriteLine(tokenResponse.Json);
+            System.Console.WriteLine("\n\n");
+
+            var client = new HttpClient();
+
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("https://localhost:5001/api/values");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                System.Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                System.Console.WriteLine(JArray.Parse(await response.Content.ReadAsStringAsync()));
+            }
+        }
+    }
+}
